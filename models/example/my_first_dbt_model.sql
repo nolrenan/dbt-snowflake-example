88f1e1584cd55ee7248819bf1afcd0175ff7f8e4
Renan Maie <rm349404@fiap.com.br>
2023-09-30 19:01:06 +0000

    inclusao da staging e marts do exemplo do snowflake

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -6,9 +6,6 @@
 
     Try changing "table" to "view" below
 */
-
-{{ config(materialized='table') }}
-
 with source_data as (
 
     select 1 as id
@@ -24,4 +21,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+ where id is not null
